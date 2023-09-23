5f670ba096381d2a8e9eec84301546164217da2f
Sushrut Deo <sushrut@seatfrog.com>
2023-09-23 20:55:19 +0100

    - Update Code

--- src/app/interfaces/ISearchTutorResponse.interface.ts
+++ src/app/interfaces/ISearchTutorResponse.interface.ts
@@ -3,6 +3,8 @@ export interface ISearchTutorResponse {
   tutorId: number;
   name: string;
   hourlyRate: number;
+  bio: string;
+  email: string;
   introduction: string;
   rating: number | null;
   tutorImage: string;
